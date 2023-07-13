3df7d77f9aa398eb7f6916c8cc33c756fbc9c1d4
esdrasilvavr <71395581+esdrasilvavr@users.noreply.github.com>
2023-07-13 20:29:15 -0300

    main
    - Ajuste de artefactId.

--- src/main/java/br/com/fatecararas/f290_dsm_tp2_piadasnerd/PiadasnerdApplication.java
+++ src/main/java/br/com/fatecararas/f290_dsm_tp2_piadasnerd/PiadasnerdApplication.java
@@ -1,4 +1,4 @@
-package br.com.fatecararas.piadasnerd;
+package br.com.fatecararas.f290_dsm_tp2_piadasnerd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/br/com/fatecararas/f290_dsm_tp2_piadasnerd/controllers/IndexController.java
+++ src/main/java/br/com/fatecararas/f290_dsm_tp2_piadasnerd/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package br.com.fatecararas.piadasnerd.controllers;
+package br.com.fatecararas.f290_dsm_tp2_piadasnerd.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
